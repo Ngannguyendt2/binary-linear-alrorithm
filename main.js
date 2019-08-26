91e36a69d9d3364ff4cef59341cf27f2cf80bf56
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-26 16:04:58 +0700

    repair file find

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let arr=[];
-let number;
+let num;
 function pushArray() {
     for (let i = 1; i <= 100; i++) {
         arr.push(i);
@@ -8,32 +8,29 @@ function pushArray() {
 }
 
 function importNumber() {
-    number = Number(prompt("Number need find"));
-    return number;
+    num = Number(prompt("Number need find"));
+    return num;
 }
 
 function linearFind() {
-    arr = pushArray();
-    number = importNumber();
-    for (let i = 0; i < arr.length; i++) {
-        if (number == arr[i]) {
+    for (let i = 0; i < arrayNumber.length; i++) {
+        if (number == arrayNumber[i]) {
             alert("Number need find at " + i + "position");
+            break;
         }
     }
 }
 
 function binaryfind() {
-    arr = pushArray();
-    number = importNumber();
     let first = 1;
-    let final = arr.length - 1;
+    let final = arrayNumber.length - 1;
     let i = 0;
     while (first <= final) {
         i = Math.floor((first + final) / 2);
-        if (arr[i] == number) {
+        if (arrayNumber[i] == number) {
             alert("Number need find at " + i + "position");
             break;
-        } else if (arr[i] < number) {
+        } else if (arrayNumber[i] < number) {
             first = i + 1;
 
         } else {
@@ -41,4 +38,6 @@ function binaryfind() {
         }
     }
 }
+let arrayNumber=pushArray();
+let number=importNumber();
 
